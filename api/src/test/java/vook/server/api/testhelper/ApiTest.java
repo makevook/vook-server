74881ae7ba856f251fb8dcc36d4d64dca9640096
seungyeop-lee <xbeast7@gmail.com>
2024-05-09 18:58:38 +0900

    feat: UTC 설정 #14

--- api/src/test/java/vook/server/api/testhelper/ApiTest.java
+++ api/src/test/java/vook/server/api/testhelper/ApiTest.java
@@ -1,13 +1,22 @@
 package vook.server.api.testhelper;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.util.TimeZone;
+
+import static vook.server.api.config.TimeZoneConfig.DEFAULT_TIME_ZONE;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class ApiTest {
 
     @Autowired
     protected TestRestTemplate rest;
-    
+
+    @BeforeEach
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(DEFAULT_TIME_ZONE));
+    }
 }
