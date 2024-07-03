c705f10be4d389fc5ef2c8925746363d5055af56
seungyeop-lee <xbeast7@gmail.com>
2024-07-03 15:56:48 +0900

    chore: Spring Data Redis PoC 작성
    Spring Data Redis를 통한 Redis 와의 통신 방법 PoC

---
+++ poc/spring-data-redis/src/main/java/vook/server/poc/springdataredis/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package vook.server.poc.springdataredis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // RedisTemplate는 직렬화 구현체를 넣지 않으면 기본적으로 JdkSerializationRedisSerializer를 사용한다.
+        // 디버깅 및 다른 언어와의 호환성을 위해 직렬화 방식을 변경한다.
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+}
---
+++ poc/spring-data-redis/src/main/java/vook/server/poc/springdataredis/usecases/TimeToLiveSupplier.java
@@ -0,0 +1,5 @@
+package vook.server.poc.springdataredis.usecases;
+
+public interface TimeToLiveSupplier {
+    long get();
+}
