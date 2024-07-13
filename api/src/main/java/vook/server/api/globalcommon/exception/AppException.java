58d6ac6dcb7c8f16d10cfe5fcb6cf37835c94de7
seungyeop-lee <xbeast7@gmail.com>
2024-07-13 16:12:36 +0900

    feat: AppException과 그 하위타입에서 예외의 cause를 담을 수 있도록 확장 #137

--- api/src/main/java/vook/server/api/globalcommon/exception/AppException.java
+++ api/src/main/java/vook/server/api/globalcommon/exception/AppException.java
@@ -1,7 +1,13 @@
 package vook.server.api.globalcommon.exception;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public abstract class AppException extends RuntimeException {
+    
+    public AppException(RuntimeException cause) {
+        super(cause);
+    }
 }
