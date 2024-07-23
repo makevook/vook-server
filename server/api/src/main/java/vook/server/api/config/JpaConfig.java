ef839d47fb0657f936315b20bdb8ec348e39013d
seungyeop-lee <xbeast7@gmail.com>
2024-07-23 12:26:53 +0900

    chore: spring-data warning 제거

--- server/api/src/main/java/vook/server/api/config/JpaConfig.java
+++ server/api/src/main/java/vook/server/api/config/JpaConfig.java
@@ -1,9 +1,17 @@
 package vook.server.api.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.keyvalue.repository.KeyValueRepository;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(
+        basePackages = "vook.server.api",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {KeyValueRepository.class})
+)
 public class JpaConfig {
 }
